2b5c2b80019b2969056b562247e3bf424e639992
svalvastic <assogames2@gmail.com>
2022-08-24 15:30:02 +0200

    ✨ feat(script): add script

--- javascript/script.js
+++ javascript/script.js
@@ -1,28 +1,42 @@
+const button = document.getElementById("calculate");
+
+button.addEventListener("click", () => {
+  const passengerName = document.getElementById("passengerName").value;
+  const eta =parseInt(document.getElementById("passengerAge").value) ;
+  const percorrenza = document.getElementById("distance").value;
+    let costoTotale = percorrenza * 0.21;
+    switch (eta) {
+        case 0:
+            costoTotale -= costoTotale * 0.2;
+            break;
+        case 2:
+            costoTotale -= costoTotale * 0.4;
+            break;
 
 
-const passengerName = document.getElementById('passengerName');
-const passengerAge = document.getElementById('passengerAge');
-
-
-
-
-let percorrenza = NaN
-let eta = NaN
-let costoTotale; 
-
-    while (isNaN(percorrenza) || isNaN(eta)) {
-        // percorrenza = parseInt(prompt("inserire il numero dei km da percorrere"));
-        // eta = parseInt( prompt("inserire l'età del passeggero"));
     }
-   costoTotale = percorrenza * 0.21;
+    costoTotale = costoTotale.toFixed(2);
 
-if (eta < 18) {
-    costoTotale -= costoTotale * 0.2;
+
+    showTicket(passengerName, eta, costoTotale)
+});
+
+
+function showTicket(passengerName, eta, costoTot) {
+    document.getElementById("viewName").innerHTML = passengerName
+    let viewRate = document.getElementById("viewRate");
+    if (eta === 1) {
+        viewRate.innerHTML = "Tariffa Standard"
+    }
+    else {
+        viewRate.innerHTML = "Tariffa Ridotta"
+    }
+
+    document.getElementById("viewCab").innerHTML = Math.floor(Math.random() * 20) + 1 
+    document.getElementById("viewCP").innerHTML = Math.floor(Math.random() * 10000) + 1 
+    document.getElementById("viewPrice").innerHTML = costoTot;
+
+
+
+
 }
-else if (eta > 65) {
-    ccostoTotale -= costoTotale * 0.4;
-}
-
-costoTotale = costoTotale.toFixed(2)
-
-// alert(`Il prezzo da pagare è €${costoTotale}`)
